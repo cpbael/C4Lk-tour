e541a6ac2b22a8e6ef9cc4d801717ff7c2982d22
cpbael <cpbael@gmail.com>
2014-04-01 14:57:20 +0800

    Initial Solver for Options and Nopts

--- solver.php
+++ solver.php
@@ -1,19 +1,17 @@
 <?php
 
-	//echo $_FILES['input']['name'];
-
+/*START OF FILE READING*/
 	if (file_exists("upload/" . $_FILES["input"]["name"]))
       {
     	unlink("upload/".$_FILES['input']['name']); //remove the file
       }
     
-  move_uploaded_file($_FILES["input"]["tmp_name"],
-  "upload/" . $_FILES["input"]["name"]); 
+ 	move_uploaded_file($_FILES["input"]["tmp_name"],
+  	"upload/" . $_FILES["input"]["name"]); 
 
 
   	$fh = fopen('upload/'. $_FILES["input"]["name"],'r') or exit("Unable to open file!");;
   	$case=(int)fgets($fh);
-  	echo $case;
   	$board;
   	$i=0;
 	for ($i=0; $i <$case ; $i++) { 
@@ -23,7 +21,45 @@
 		}
 	}
 	fclose($fh);  
-
+	//echo sizeof($board[0]);
 	var_dump($board); 
+/*END OF FILE READING*/
+
+/*GENERATE CHILDREN FOR EACH TILE*/
+for($i=0;$i<$case;$i++){
+	for ($j=0; $j < sizeof($board[$i]) ; $j++) { 
+		$children[$i][$]
+	}
+}
+
+
+/*OPTIONS AND NOPTS*/
+	$start=$move=0;
+
+	$nopts[$start]=1;
+
+	while($nopts[$start] > 0){
+
+		if($nopts[$move] > 0){	//populate possible moves
+			
+			$nopts[++$move]=0;
+
+			if($move > sizeof($board[0])){ //solution found
+				for ($i=1; $i < $move ; $i++) { 
+					$solution[][$i] = $options[$i][$nopts[$i]];
+				}
+			}else{
+				for ($candidate=0; $i < ; $i++) { 
+					# code...
+				}
+			}
+
+		}
+
+	}
+
+
+/*OPTIONS AND NOPTS*/
+
 
 ?>
